76c91af49432db864a34b2af490e58147b3dab7d
Kyluuuu <xthegrimcreeper@gmail.com>
2024-12-08 18:08:57 +1100

    Updates

--- assets/threed-DXrqp-16.js
+++ assets/threed-DXrqp-16.js
@@ -1,4 +1,4 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";/* empty css                 *//**
+import"./mainCSS-Z_oY0_b-.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
